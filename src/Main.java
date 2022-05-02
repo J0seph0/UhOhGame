4b6bdad43f5656f04066ba807fa496b68e8e90df
J0seph0 <wnefG5TKnP3F8G6>
2022-05-02 11:37:53 -0700

    test

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame mainFrame = new JFrame();
+        //settings for window
+        mainFrame.setSize(500,500);
+        mainFrame.setVisible(true);
+    }
+
+
+}
---
+++ src/MainFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class MainFrame {
+    private JTextField textField1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
