9a009acdeea034e9877670ea9489f0f25ad2a3cc
J0seph0 <jhoward26@ewu.edu>
2022-05-03 10:29:14 -0700

    -Changed the layout of the gui as well as made it do a cool little repeating thing. -Then started to add a generic controller to complete actions for the button presses in the gui

---
+++ src/GenericController.java
@@ -0,0 +1,11 @@
+import Command.Command;
+
+public class GenericController {
+    public Command action;
+
+    public GenericController(){}
+
+    public void setAction(Command action) {
+        this.action = action;
+    }
+}
