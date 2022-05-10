f41bf1f5957f7c67c0d5baaad6437a17f7522261
J0seph0 <wnefG5TKnP3F8G6>
2022-05-10 10:32:50 -0700

    working correct

--- src/Classes/Player.java
+++ src/Classes/Player.java
@@ -1,9 +1,24 @@
 package Classes;
 
 import Observer.Observable;
+import Observer.Observer;
 
 import java.util.ArrayList;
 
 public class Player implements Observable {
 
+    @Override
+    public void addObserver(Observer o) {
+
+    }
+
+    @Override
+    public void notifyObservers() {
+
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+
+    }
 }
