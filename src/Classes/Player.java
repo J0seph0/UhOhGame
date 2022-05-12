03838f25575d96de58119235bcfcda767a1626d4
Angel2KEWU <amejia9@ewu.edu>
2022-05-12 10:49:39 -0700

    Achievements 2/3 Done

--- src/Classes/Player.java
+++ src/Classes/Player.java
@@ -7,18 +7,28 @@ import java.util.ArrayList;
 
 public class Player implements Observable {
 
-    @Override
-    public void addObserver(Observer o) {
+    private String name;
 
+    private final ArrayList<Observer> observers;
+
+    public Player() {
+        observers = new ArrayList<Observer>();
     }
 
     @Override
-    public void notifyObservers() {
+    public void addObserver(Observer o) {
+        observers.add(o);
+    }
 
+    @Override
+    public void notifyObservers(Event ev) {
+        for (Observer o: observers) {
+            o.OnNotify(ev);
+        }
     }
 
     @Override
     public void removeObserver(Observer o) {
-
+        observers.remove(o);
     }
 }
--- src/Methods/Methods.java
+++ src/Methods/Methods.java
@@ -1,4 +1,5 @@
 package Methods;
+import Classes.*;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
@@ -10,6 +11,7 @@ import javax.swing.*;
 import java.util.ArrayList;
 
 public class Methods {
+
 
     JButton[] jButtons = new JButton[9];
 
@@ -35,6 +37,9 @@ public class Methods {
     //method for determining if someone has won by checking some conditions
     public static boolean determineWinner(ArrayList<String> buttons){
 //We need to check 3 locations, indexes: 0, 4, 8
+        Player p1 = new Player(); // Player 1 is X
+        Achievements achievements = new Achievements();
+        p1.addObserver(achievements);
 
 //check for the top left corner
         if((buttons.get(0).equals(buttons.get(1)) && buttons.get(0).equals(buttons.get(2))) && buttons.get(0) != "" ||
@@ -42,6 +47,11 @@ public class Methods {
             System.out.println(buttons.get(0) + " is the winner!");
             //winner window
             setUpWinnerWindow(new JFrame("Winner Window"));
+            if(buttons.get(0).equals("X")) {
+                achievements.OnNotify(Event.PLAYER_WINS);
+            } else {
+                achievements.OnNotify(Event.PLAYER_LOSES);
+            }
             return true;
         }
 
@@ -49,6 +59,11 @@ public class Methods {
         if((buttons.get(8).equals(buttons.get(7))  && buttons.get(8).equals(buttons.get(6))) & buttons.get(8) != ""  ||
                 (buttons.get(8).equals(buttons.get(5))  && buttons.get(8).equals(buttons.get(2)) && buttons.get(8) != "")) {
             System.out.println(buttons.get(8) + " is the winner!");
+            if(buttons.get(8).equals("X")) {
+                achievements.OnNotify(Event.PLAYER_WINS);
+            } else {
+                achievements.OnNotify(Event.PLAYER_LOSES);
+            }
             return true;
 
         }
@@ -59,6 +74,11 @@ public class Methods {
                 (buttons.get(1).equals(buttons.get(4))  && buttons.get(7).equals(buttons.get(4)) && buttons.get(4) != "") ||
                 (buttons.get(3).equals(buttons.get(4))  && buttons.get(5).equals(buttons.get(4)) && buttons.get(4) != "")) {
             System.out.println(buttons.get(4) + " is the winner!");
+            if(buttons.get(4).equals("X")) {
+                achievements.OnNotify(Event.PLAYER_WINS);
+            } else {
+                achievements.OnNotify(Event.PLAYER_LOSES);
+            }
             return true;
         }
         return false;
